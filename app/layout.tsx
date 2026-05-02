4582f3e9322cd2d16d3dc0d77d765f8c03a9cf6d
João Vitor <joaovirorconceicaodesoza21@gmail.com>
2026-05-02 00:29:35 -0300

    fix: corrigido linguagem do site html

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="pt-BR"
       className={cn('dark', "h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable, 'scroll-smooth')}
     >
       <body className="min-h-full flex flex-col">{children}</body>
